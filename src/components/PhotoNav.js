ea05cd42fa8db117f89cb3d20328572c15b432d5
SMB <sethmichaelbrown@gmail.com>
2019-02-19 14:58:26 -0700

    Start of photo nav

--- src/components/PhotoNav.js
+++ src/components/PhotoNav.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import '../App.css';
+import { Route, Link } from 'react-router-dom'
+import recording from '../images/recording.jpg'
+import singer from '../images/singer.png'
+import soloist from '../images/soloist.png'
+
+import '../App.css'
 
 
 const PhotoNav = (props) => {
 
   return (
     <div className='PhotoNav'>
-      
+      <div className="photonav-flex">
+
+        <div className="item-1" id='photo-1'>
+          <img className='photonav-item' src={recording} alt="" />
+        </div>
+
+        <div className="item-2" id='photo-2'>
+          <img className='photonav-item' src={singer} alt="" />
+        </div>
+
+        <div className="item-3" id='photo-3'>
+          <img className='photonav-item' src={soloist} alt="" />
+        </div>
+
+      </div>
 
     </div>
 
