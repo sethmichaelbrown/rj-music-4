5f984c60c9d3f3b7438ec961f845c053c2b7b1c7
SMB <sethmichaelbrown@gmail.com>
2019-02-19 08:55:13 -0700

    Basic navigation in place via browswer router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-import About from './components/About'
-import Home from './components/Home'
-import Topics from './components/Topics'
+// import About from './components/About'
+// import Home from './components/Home'
+// import Topics from './components/Topics'
+import NavBar from './components/NavBar'
+import PhotoNav from './components/PhotoNav'
 
 import './App.css'
 
 class App extends Component {
+
+  state = {
+    tester: true
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/topics">Topics</Link>
-              </li>
-            </ul>
-
-            <hr />
-
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/topics" component={Topics} />
-          </div>
-
+          <NavBar />
+          <PhotoNav />
 
         </div>
       </Router>
