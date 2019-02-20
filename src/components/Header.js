6fa43afabc7f653ffe9d5df5e117ff39444d9b1a
SMB <sethmichaelbrown@gmail.com>
2019-02-20 11:08:10 -0700

    Stash commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../App.css';
+
+
+const Header = (props) => {
+
+  return (
+    <div className='Header'>
+      <div className="row container">
+        <div className="col-12 py-3">
+          <h2>Robert James Music - Create, Express, and Record Your Own Music </h2>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Header
